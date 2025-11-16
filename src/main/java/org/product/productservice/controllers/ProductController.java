6f32a6a1cd5022c2f4ebccfa81f6d4109b1e0fcf
sapna <nagwanshisapna@gmail.com>
2025-11-16 12:14:39 +0530

    Remove unused imports

--- src/main/java/org/product/productservice/controllers/ProductController.java
+++ src/main/java/org/product/productservice/controllers/ProductController.java
@@ -1,13 +1,9 @@
 package org.product.productservice.controllers;
 
-import org.product.productservice.DTOS.ExceptionDto;
-import org.product.productservice.DTOS.FakeStoreProductDto;
 import org.product.productservice.DTOS.GenericProductDto;
 import org.product.productservice.exceptions.ProductNotFoundException;
 import org.product.productservice.services.ProductService;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
